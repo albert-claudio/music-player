e6e986ec47d7e58d0264acd800139834c2130d62
Albert Claudio <albertclaudiolink@gmail.com>
2023-08-03 19:20:00 -0300

    Music player

---
+++ index.js
@@ -0,0 +1,102 @@
+const image = document.getElementById('cover'),
+    title = document.getElementById('music-title'),
+    artist = document.getElementById('music-artist'),
+    currentTimeEl = document.getElementById('current-time'),
+    durationEl = document.getElementById('duration'),
+    progress = document.getElementById('progress'),
+    playerProgress = document.getElementById('player-progress'),
+    prevBtn = document.getElementById('prev'),
+    nextBtn = document.getElementById('next'),
+    playBtn = document.getElementById('play'),
+    background = document.getElementById('bg-img');
+
+const music = new Audio();
+
+const songs = [
+    {
+        path: 'assets/THE SPECTACULAR SPIDER-MAN - Main Theme By Joey Medina _ Disney XD.mp3',
+        displayName: 'The Spectacular Spider Man Theme Song',
+        cover: 'assets/Pin by Pinner on spider-verse_ _ Marvel phone wallpaper, Spiderman artwork, Marvel wallpaper.jpg',
+        artist: 'Joey Medina',
+    },
+    {
+        path: 'assets/[FREE] J.I.D x J Cole Type Beat - _Ever Know_.mp3',
+        displayName: 'Ever Know',
+        cover: 'assets/Jazz wallpaper_.jpg',
+        artist: 'J.I.D',
+    },
+    {
+        path: 'assets/Eminem - Without Me (Official Music Video).mp3',
+        displayName: 'Without Me',
+        cover: 'assets/Eminem - Without Me (Official Music Video) - 1280x720.jpg',
+        artist: 'Eminem',
+    }
+];
+
+let musicIndex = 0;
+let isPlaying = false;
+
+function togglePlay() {
+    if (isPlaying) {
+        pauseMusic();
+    } else {
+        playMusic();
+    }
+}
+
+function playMusic() {
+    isPlaying = true;
+    // Change play button icon
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    // Set button hover title
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+function pauseMusic() {
+    isPlaying = false;
+    // Change pause button icon
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    // Set button hover title
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+function loadMusic(song) {
+    music.src = song.path;
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    image.src = song.cover;
+    background.src = song.cover;
+}
+
+function changeMusic(direction) {
+    musicIndex = (musicIndex + direction + songs.length) % songs.length;
+    loadMusic(songs[musicIndex]);
+    playMusic();
+}
+
+function updateProgressBar() {
+    const { duration, currentTime } = music;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+}
+
+function setProgressBar(e) {
+    const width = playerProgress.clientWidth;
+    const clickX = e.offsetX;
+    music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener('click', togglePlay);
+prevBtn.addEventListener('click', () => changeMusic(-1));
+nextBtn.addEventListener('click', () => changeMusic(1));
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate', updateProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
+
+loadMusic(songs[musicIndex]);
